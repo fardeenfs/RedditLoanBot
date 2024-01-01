faf7eb65ef4a3c983788f46bbae45038a652edca
Fardeen Faisal <68156295+fardeenfs@users.noreply.github.com>
2023-12-31 17:27:39 -0700

    heroku settings

--- LoanServer/settings.py
+++ LoanServer/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,6 +96,10 @@ DATABASES = {
     }
 }
 
+
+django_heroku.settings(locals())
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
