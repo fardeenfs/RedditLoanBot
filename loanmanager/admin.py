28a24e96b5c4f779eb24b009bbdd2260d107a55c
Fardeen Faisal <fardeenfaisal.fs@gmail.com>
2025-03-05 00:43:02 -0500

    Update admin.py

--- loanmanager/admin.py
+++ loanmanager/admin.py
@@ -15,9 +15,9 @@ class LoanAdmin(admin.ModelAdmin):
 
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_filter = ('payment_date')  # Filter by currency and loan association
-    list_display = ('id', 'loan_id', 'loan', 'amount', 'currency', 'payment_date')  # Show loan ID in the list
-    search_fields = ('loan__loan_id', 'loan__lender__username', 'loan__borrower__username')  # Search by loan ID or usernames
+    list_filter = ('payment_date',)  # Filter by currency and loan association
+    list_display = ('payment_id_display', 'loan_id', 'loan', 'amount', 'currency', 'payment_date')  # Show loan ID in the list
+    search_fields = ('payment_id_display', 'loan__loan_id', 'loan__lender__username', 'loan__borrower__username')  # Search by loan ID or usernames
 
     def loan_id(self, obj):
         return obj.loan.loan_id  # Display loan ID in list
